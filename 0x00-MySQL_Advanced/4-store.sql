4d2bc72ddbfe324e3df74abd894b7e9cdf7b5038
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-18 00:44:35 +0200

    4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+# decreases the quantity of an item after adding a new order
+DROP TRIGGER IF EXISTS after_orders_insert;
+CREATE TRIGGER after_orders_insert 
+AFTER INSERT ON orders 
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE items.name = NEW.item_name;
