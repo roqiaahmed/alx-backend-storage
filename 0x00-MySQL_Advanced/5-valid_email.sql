e1a67c6c96f745765eed5fb88438e640c759dbbc
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-18 01:12:28 +0200

    5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- valid_email
+DELIMITER $$
+CREATE TRIGGER valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;
