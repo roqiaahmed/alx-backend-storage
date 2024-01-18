88732919c70b4b224eb8e735f38b5b7a9cd3a373
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-18 14:15:06 +0200

    11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW IF NOT EXISTS need_meeting AS
 SELECT name FROM students
-WHERE score < 80 AND (last_meeting = NULL OR MONTH(last_meeting) > MONTH(CURDATE()));
+WHERE score < 80 AND (last_meeting = NULL OR last_meeting < ADDDATE(NOW(), INTERVAL -1 MONTH));
