ea712fb2b7d1e77bd314352b7dd055ea1481d058
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-22 17:26:42 +0200

    fix task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,5 +6,4 @@
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
 
-    topic_list = mongo_collection.find({"topics": topic})
-    return topic_list.list()
+    return mongo_collection.find({"topics": topic})
