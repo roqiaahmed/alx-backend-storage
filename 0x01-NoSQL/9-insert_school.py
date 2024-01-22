54a01022758a8d72180680954c9520bc1215d546
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-22 17:18:01 +0200

    fix task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    new_doc = mongo_collection.insert(kwargs)
+    new_doc = mongo_collection.insert_one(kwargs)
     return new_doc.inserted_id
